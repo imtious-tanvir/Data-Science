91a4d724f5036d94f29953ed43a972359af81e26
Imtious tanvir <82751228+imtious-tanvir@users.noreply.github.com>
2022-08-10 22:35:40 +0600

    Add files via upload

---
+++ measure of spread standard deviation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+dataset = np.random.randint(5, 20, 5)
+print("Dataset is ",dataset)
+
+# variance = np.var(dataset)
+# print("Variance is ",variance)
+
+standard_deviation = np.std(dataset)
+print("Standard deviation is ",standard_deviation)
